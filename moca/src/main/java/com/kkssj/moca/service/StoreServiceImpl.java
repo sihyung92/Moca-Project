f784b16b6931be2fab4d726db370e4663eaefc3c
Song Hae <thdgo456@naver.com>
2019-09-20 17:58:01 +0900

    AOP 트랙잭션 처리

--- moca/src/main/java/com/kkssj/moca/service/StoreServiceImpl.java
+++ moca/src/main/java/com/kkssj/moca/service/StoreServiceImpl.java
@@ -19,6 +19,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kkssj.moca.model.AccountDao;
