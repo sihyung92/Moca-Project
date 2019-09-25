5dda22fdc65d861b6279148076e87f77772ae92f
Jinhui Kim <xzdfor@naver.com>
2019-09-25 12:18:46 +0900

    네이버 계정으로 이미지 수정시 에러 문제 해결

--- moca/src/main/java/com/kkssj/moca/service/MypageServiceImpl.java
+++ moca/src/main/java/com/kkssj/moca/service/MypageServiceImpl.java
@@ -1,4 +1,4 @@
-package com.kkssj.moca.service;
+﻿package com.kkssj.moca.service;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -232,12 +232,13 @@ public class MypageServiceImpl implements MypageService{
 				//db에서 원래 있던 이미지 url select로 받아오기
 				AccountVo OldImgUrlVo = accountDao.selectProfileImageByaccountId(editAccountVo.getAccount_id());
 				if(OldImgUrlVo.getProfileImage()!=null) {
-					if(!OldImgUrlVo.getProfileImage().contains("kakaocdn")) {					
-						DelimageVo.setDelImageVo(OldImgUrlVo.getProfileImage());
-						
-						//s3에서 이미지 삭제 (카카오톡 url이 아닐때)
-						s3.fileDelete(DelimageVo.getPath()+"/"+DelimageVo.getFileName());
-						s3.fileDelete(DelimageVo.getPath()+"/"+DelimageVo.getThumbnailFileName());
+					if(!OldImgUrlVo.getProfileImage().contains("kakaocdn")) {	
+						if(OldImgUrlVo.getProfileImage().length() !=0) {
+							DelimageVo.setDelImageVo(OldImgUrlVo.getProfileImage());
+							//s3에서 이미지 삭제 (카카오톡 url이 아닐때)
+							s3.fileDelete(DelimageVo.getPath()+"/"+DelimageVo.getFileName());
+							s3.fileDelete(DelimageVo.getPath()+"/"+DelimageVo.getThumbnailFileName());		
+						}				
 					}
 				}
 				
